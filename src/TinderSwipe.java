2efb9fcd9aeaa1a962e457df6137fd3e0556b183
Robyn Lesch <r.lesch97@gmail.com>
2020-08-07 10:51:41 +0200

    First Upload

---
+++ src/TinderSwipe.java
@@ -0,0 +1,35 @@
+public class TinderSwipe {
+    Profile[] rightSwipes;
+    Profile[] preference;
+
+    Database userData;
+    Profile current;
+    private int tracker = -1;
+    private int right = 0;
+
+	public TinderSwipe(String preference) {
+        //this.preference = preference;
+        rightSwipes = new Profile[5];
+
+        userData = new Database();
+        userData.readCSV();
+        userData.setPreference(preference);
+    
+        this.preference = userData.getPreference();
+    }
+
+
+	public Profile getNext() {
+        tracker ++;
+        current = preference[tracker];
+        return current;
+	}
+	public void right() {
+        rightSwipes[right] = current;
+        userData.writeCSV(current);
+    }
+
+    public Profile[] getRightSwipes(){
+        return rightSwipes;
+    }
+}
